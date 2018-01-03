223d06bacede8562a2c32028522624d51d7ed302
QueensKid <andres.ibarra0716@gmail.com>
2018-01-02 17:36:42 -0800

    Need to work on logic for app

---
+++ LAB06Interfaces/LAB06Interfaces/Classes/IDamages.cs
@@ -0,0 +1,6 @@
+﻿namespace LAB06Interfaces
+{
+    internal interface IDamages
+    {
+    }
+}
--- LAB06Interfaces/LAB06Interfaces/Classes/SpecialAbility.cs
+++ LAB06Interfaces/LAB06Interfaces/Classes/SpecialAbility.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using LAB06Interfaces.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LAB06Interfaces
 {
-    class SpecialAbility : Teams
+    class SpecialAbility : IDamage, IHeal
     {
+        public int Damage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Restore { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void CausePain()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoreHealth()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
